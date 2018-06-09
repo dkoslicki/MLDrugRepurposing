2de0a5d846d3724f433599ffd5cfc54a6a823d55
dkoslicki <dmkoslicki@gmail.com>
2018-06-08 17:58:15 -0700

    To tabs

--- code/Classify.py
+++ code/Classify.py
@@ -29,6 +29,10 @@ np.random.seed(int(time.time()/100))
 
 # SemmedDB plus NDF, do nothing
 
+# Just NDF
+ImportData.TP_files = ['../data/NDF_TP_curie.csv']
+ImportData.TN_files = ['../data/NDF_TN_curie.csv']
+
 # Import the data
 X, y, id_list = ImportData.import_data()
 print(len(X))
@@ -41,11 +45,11 @@ print(np.unique(y, return_counts=True))
 
 #model = GaussianNB()
 
-#model = RandomForestClassifier(class_weight='balanced', n_estimators=10, max_depth=10)  # Doesn't over-fit
+model = RandomForestClassifier(class_weight='balanced', n_estimators=10, max_depth=10)  # Doesn't over-fit with max_depth=5 and just semmeddb data
 
 #model = VotingClassifier(estimators=[('lr', model1), ('gnb', model2), ('rf', model3)], voting='soft')
 
-model = AdaBoostClassifier(n_estimators=100)
+#model = AdaBoostClassifier(n_estimators=100)
 
 # for the cross fold validation
 cv = ms.StratifiedKFold(n_splits=10, random_state=random_state, shuffle=True)
@@ -65,23 +69,23 @@ np.random.shuffle(shuffled_idx)
 test_f1_mean = np.mean(ms.cross_val_score(model, X[shuffled_idx], y[shuffled_idx], cv=10, n_jobs=-1, scoring='f1'))
 print('using cross val score F1 = %0.4f' % (test_f1_mean))
 for train, test in cv.split(X, y):
-    model_i = model.fit(X[train], y[train])
-    probas_ = model_i.predict_proba(X[test])
-    pred = model_i.predict(X[test])
-    f1 = met.f1_score(y[test], pred, average='binary')
-    f1s.append(f1)
-    # Compute ROC curve and area the curve
-    fpr, tpr, thresholds = met.roc_curve(y[test], probas_[:, 1])
-    tprs.append(sci.interp(mean_fpr, fpr, tpr))
-    tprs[-1][0] = 0.0
-    roc_auc = met.auc(fpr, tpr)
-    aucs.append(roc_auc)
-    plt.plot(fpr, tpr, lw=1, alpha=0.3,
-            label='ROC fold %d (AUC = %0.4f, F1 = %0.4f)' % (i, roc_auc, f1))
+	model_i = model.fit(X[train], y[train])
+	probas_ = model_i.predict_proba(X[test])
+	pred = model_i.predict(X[test])
+	f1 = met.f1_score(y[test], pred, average='binary')
+	f1s.append(f1)
+	# Compute ROC curve and area the curve
+	fpr, tpr, thresholds = met.roc_curve(y[test], probas_[:, 1])
+	tprs.append(sci.interp(mean_fpr, fpr, tpr))
+	tprs[-1][0] = 0.0
+	roc_auc = met.auc(fpr, tpr)
+	aucs.append(roc_auc)
+	plt.plot(fpr, tpr, lw=1, alpha=0.3,
+			label='ROC fold %d (AUC = %0.4f, F1 = %0.4f)' % (i, roc_auc, f1))
 
-    i += 1
+	i += 1
 plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
-        label='Coin Flip', alpha=.8)
+		label='Coin Flip', alpha=.8)
 
 mean_tpr = np.mean(tprs, axis=0)
 mean_f1 = np.mean(f1s)
@@ -89,15 +93,15 @@ mean_tpr[-1] = 1.0
 mean_auc = met.auc(mean_fpr, mean_tpr)
 std_auc = np.std(aucs)
 plt.plot(mean_fpr, mean_tpr, color='b',
-        label=u'Mean ROC (AUC = %0.4f \u00B1 %0.4f, \n        \
-            Mean F1 = %0.4f)' % (mean_auc, std_auc, mean_f1),
-        lw=2, alpha=.8)
+		label=u'Mean ROC (AUC = %0.4f \u00B1 %0.4f, \n        \
+			Mean F1 = %0.4f)' % (mean_auc, std_auc, mean_f1),
+		lw=2, alpha=.8)
 
 std_tpr = np.std(tprs, axis=0)
 tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
 tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
 plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-                label=r'$\pm$ 1 std. dev.')
+				label=r'$\pm$ 1 std. dev.')
 
 plt.xlim([-0.05, 1.05])
 plt.ylim([-0.05, 1.05])
@@ -122,15 +126,15 @@ all_df = pd.read_csv(comb).sample(frac=1/500).reset_index(drop=True)
 
 x_i = []
 for row in range(20):
-    try:
-        source_id = map_dict[all_df['drug'][row]]
-        target_id = map_dict[all_df['disease'][row]]
+	try:
+		source_id = map_dict[all_df['drug'][row]]
+		target_id = map_dict[all_df['disease'][row]]
 
-        print([all_df['drug'][row],all_df['disease'][row]])
-    except KeyError:
-        continue
+		print([all_df['drug'][row],all_df['disease'][row]])
+	except KeyError:
+		continue
 
-    x_i += [list(node_vec.iloc[source_id,1:]) + list(node_vec.iloc[target_id,1:])]
+	x_i += [list(node_vec.iloc[source_id,1:]) + list(node_vec.iloc[target_id,1:])]
 
 
 ex.joblib.dump(model, 'Lreg1.pkl')
