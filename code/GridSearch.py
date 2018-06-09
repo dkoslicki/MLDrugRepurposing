f1e984ed497a93ec9e716bbb171d403e44f6520f
dkoslicki <david.koslicki@math.oregonstate.edu>
2018-06-09 00:45:07 -0700

    Setup GridSearch for cluster

--- code/GridSearch.py
+++ code/GridSearch.py
@@ -4,6 +4,8 @@ import numpy as np
 import ImportData
 from sklearn.model_selection import GridSearchCV
 import sklearn.model_selection as ms
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 ImportData = ImportData.ImportData()
 
@@ -29,19 +31,14 @@ np.random.seed(int(time.time()/100))
 X, y, id_list = ImportData.import_data()
 
 model = RandomForestClassifier(class_weight='balanced')
-#param_grid = {'n_estimators': [10, 50, 100, 1000],
-#			'max_features': ['auto', 10, 25, 50, 100],
-#			'max_depth': [10, 20, 50],
-#			'min_samples_split': [2, 10, 50, 100],
-#			'min_samples_leaf': [1, 10, 100, 1000],
-#			}
-
-param_grid = {'n_estimators': [10, 50],
-			'max_features': ['auto', 10],
-			'max_depth': [10],
-			'min_samples_split': [2],
-			'min_samples_leaf': [1],
+param_grid = {'n_estimators': [10, 50, 100, 1000],
+			'max_features': ['auto', 10, 25, 50, 100],
+			'max_depth': [None, 10, 20, 50],
+			'min_samples_split': [2, 10, 50, 100],
+			'min_samples_leaf': [1, 10, 100, 1000],
+			'max_leaf_nodes': [None, 10, 100, 1000]
 			}
+
 
 cv = ms.StratifiedKFold(n_splits=10, random_state=random_state, shuffle=True)
 
@@ -52,3 +49,26 @@ res = clf.fit(X, y)
 print(clf.best_score_)
 
 print(clf.best_estimator_)
+
+print("Best parameters set found on development set:")
+print()
+print(clf.best_params_)
+print()
+print("Grid scores on development set:")
+print()
+means = clf.cv_results_['mean_test_score']
+stds = clf.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+	print("%0.3f (+/-%0.03f) for %r"
+		  % (mean, std * 2, params))
+print()
+
+print("Detailed classification report:")
+print()
+print("The model is trained on the full development set.")
+print("The scores are computed on the full evaluation set.")
+print()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+y_true, y_pred = y_test, clf.predict(X_test)
+print(classification_report(y_true, y_pred))
+print()
