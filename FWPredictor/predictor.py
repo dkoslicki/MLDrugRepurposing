3d25100c34e6e2548710e39b079f3b9894384ebb
Finn Womack <flan313@gmail.com>
2018-06-29 09:52:48 -0700

    Added single prediction methods

--- FWPredictor/predictor.py
+++ FWPredictor/predictor.py
@@ -1,6 +1,14 @@
-import sklearn.linear_model as lm
 import pandas as pd
 import numpy as np
+import scipy as sci
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import sklearn as skl
+import sklearn.linear_model as lm
+import sklearn.externals as ex
+import sklearn.metrics as met
+import sklearn.model_selection as ms
+import time
 from sklearn.externals import joblib
 from sklearn.ensemble import RandomForestClassifier
 
@@ -8,6 +16,8 @@ class predictor():
 
     def __init__(self, model_file = 'LogModel.pkl'):
         self.model = joblib.load(model_file)
+        self.graph = None
+        self.X = None
 
     def prob(self, X):
         return self.model.predict_proba(X)
@@ -20,32 +30,39 @@ class predictor():
         self.graph = graph.sort_values(0).reset_index(drop=True)
         self.map_df = pd.read_csv(map_file, index_col=None)
 
-        data = pd.read_csv(file, index_col=None)
+        if file is not None:
+            data = pd.read_csv(file, index_col=None)
 
-        map_dict = {}
-        X_list = []
-        drop_list = []
-        for row in range(len(data)):
-            source_id = self.map_df.loc[self.map_df['curie'] == data['source'][row], 'id']
-            target_id = self.map_df.loc[self.map_df['curie'] == data['target'][row], 'id']
-            if len(source_id) >0 and len(target_id)>0:
-                source_id = source_id.iloc[0]
-                target_id = target_id.iloc[0]
-                X_list += [list(self.graph.iloc[source_id,1:]) + list(self.graph.iloc[target_id,1:])]
-            else:
-                drop_list += [row]
+            map_dict = {}
+            X_list = []
+            drop_list = []
+            for row in range(len(data)):
+                source_id = self.map_df.loc[self.map_df['curie'] == data['source'][row], 'id']
+                target_id = self.map_df.loc[self.map_df['curie'] == data['target'][row], 'id']
+                if len(source_id) >0 and len(target_id)>0:
+                    source_id = source_id.iloc[0]
+                    target_id = target_id.iloc[0]
+                    X_list += [list(self.graph.iloc[source_id,1:]) + list(self.graph.iloc[target_id,1:])]
+                else:
+                    drop_list += [row]
 
-        print(drop_list)
-
-        self.X = np.array(X_list)
-        self.data = data.drop(data.index[drop_list]).reset_index(drop=True)
-        self.dropped_data = data.iloc[drop_list].reset_index(drop=True)
+            self.X = np.array(X_list)
+            self.data = data.drop(data.index[drop_list]).reset_index(drop=True)
+            self.dropped_data = data.iloc[drop_list].reset_index(drop=True)
 
     def prob_file(self):
-        return self.prob(self.X)
+        if self.X is None:
+            print('Error: Must first run predictor.import_file(<filename>) before calling this method')
+            return None
+        else:
+            return self.prob(self.X)
 
     def predict_file(self):
-        return self.predict(self.X)
+        if self.X is None:
+            print('Error: Must first run predictor.import_file(<filename>) before calling this method')
+            return None
+        else:
+            return self.predict(self.X)
 
     def build_pred_df(self):
         probs = self.prob_file()
@@ -65,6 +82,42 @@ class predictor():
         df = df.sort_values('treat_prob', ascending = False).reset_index(drop=True)
         return df
 
+    def predict_single(self, source_curie, target_curie):
+        if self.graph is None:
+            self.import_file(None)
+        source_id = self.map_df.loc[self.map_df['curie'] == source_curie, 'id']
+        target_id = self.map_df.loc[self.map_df['curie'] == target_curie, 'id']
+        if len(source_id) >0 and len(target_id)>0:
+            source_id = source_id.iloc[0]
+            target_id = target_id.iloc[0]
+            X = np.array([list(self.graph.iloc[source_id,1:]) + list(self.graph.iloc[target_id,1:])])
+            return self.predict(X)
+        elif len(source_id) >0:
+            print(target_curie + ' was not in the largest connected component of graph.')
+        elif len(target_id)>0:
+            print(source_curie + ' was not in the largest connected component of graph.')
+        else:
+            print(source_curie + ' and ' + target_curie + ' were not in the largest connected component of graph.')
+        return None
+
+    def prob_single(self, source_curie, target_curie):
+        if self.graph is None:
+            self.import_file(None)
+        source_id = self.map_df.loc[self.map_df['curie'] == source_curie, 'id']
+        target_id = self.map_df.loc[self.map_df['curie'] == target_curie, 'id']
+        if len(source_id) >0 and len(target_id)>0:
+            source_id = source_id.iloc[0]
+            target_id = target_id.iloc[0]
+            X = np.array([list(self.graph.iloc[source_id,1:]) + list(self.graph.iloc[target_id,1:])])
+            return self.prob(X)[:,1]
+        elif len(source_id) >0:
+            print(target_curie + ' was not in the largest connected component of graph.')
+        elif len(target_id)>0:
+            print(source_curie + ' was not in the largest connected component of graph.')
+        else:
+            print(source_curie + ' and ' + target_curie + ' were not in the largest connected component of graph.')
+        return None
+
     def test(self):
         self.import_file('test_set.csv')
         print('df w/o nodes not in largest connected component:')
@@ -76,10 +129,17 @@ class predictor():
         print('-------------------------------------------------')
         df_all = self.build_pred_df_all()
         print(df_all)
-
-#if __name__ == '__main__':
-#	pr = predictor()
-#	pr.test()
+        
+    def single_test(self):
+        print(self.predict_single('ChEMBL:154','DOID:8398'))
+        print(self.predict_single('ChEMBL:154','HP:0002758'))
+        print(self.predict_single(':D','DOID:8398'))
+        print(self.predict_single('ChEMBL:154',':D'))
+        print('-------------------------------------------')
+        print(self.prob_single('ChEMBL:154','DOID:8398'))
+        print(self.prob_single('ChEMBL:154','HP:0002758'))
+        print(self.prob_single(':D','DOID:8398'))
+        print(self.prob_single('ChEMBL:154',':D'))
 
 
 
